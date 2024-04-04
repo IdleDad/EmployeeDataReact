633966fe32fdf2671ee4aa348c6325af721bd474
Yashwanth K <87516825+IdleDad@users.noreply.github.com>
2024-04-04 18:45:35 -0400

    MyFirstCommit

---
+++ src/components/title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './titlestyle.css'
+const Title = (props) => {
+    return (
+        <h1> Name: {props.firstName} {props.lastName} </h1> 
+    );
+}
+
+export default Title;
